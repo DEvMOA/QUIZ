e7b75e424ab16a641895b51200b0444727f0faae
DEvMOA <moabdourahmane42@gmail.com>
2024-10-23 17:05:34 +0100

    Clean rreact app

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,17 @@
+export function Quiz(){
+    return(
+        <div className="container">
+            <h1>Quiz App</h1>
+            <hr></hr>
+            <h2>1. Which device is required for the internet connection?</h2>
+            <ul>
+                <li>Modem</li>
+                <li>Router</li>
+                <li>LAN Cable</li>
+                <li>Pen Drive</li>
+            </ul>
+            <button>Next</button>
+            <p>1of5 questions</p>
+        </div>
+    )
+}
