1bad09a58be373ce16745d7991836ef3cc70c0bd
DEvMOA <moabdourahmane42@gmail.com>
2024-10-24 21:39:02 +0100

    Add framer-motion

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,9 @@
-import { useRef, useState } from "react"
-import "./Quiz.css"
-import { quiz } from "../assets/quiz"
+import { useRef, useState } from "react";
+import "./Quiz.css";
+import { quiz } from "../assets/quiz";
+import { motion } from "framer-motion";
 
-export function Quiz(){
+export function Quiz() {
     const [index, setIndex] = useState(0);
     const [question, setQuestion] = useState(quiz[index]);
     const [score, setScore] = useState(0);
@@ -15,43 +16,43 @@ export function Quiz(){
     let Option4 = useRef(null);
     let option_array = [Option1, Option2, Option3, Option4];
 
-    const checkAns = (e,ans)=>{
-        if(lock===false){
-            if(index === quiz.length - 1){
+    const checkAns = (e, ans) => {
+        if (lock === false) {
+            if (index === quiz.length - 1) {
                 setResult(true);
             }
-            if(question.ans===ans){
+            if (question.ans === ans) {
                 e.target.classList.add("correct");
-                setLock(true)
-                setScore(score => score + 1)
-            }else{
+                setLock(true);
+                setScore((score) => score + 1);
+            } else {
                 e.target.classList.add("wrong");
-                setLock(true)
-                option_array[question.ans-1].current.classList.add("correct");
+                setLock(true);
+                option_array[question.ans - 1].current.classList.add("correct");
             }
         }
-    }
+    };
 
-    const next = ()=>{
-        if(lock===true){
+    const next = () => {
+        if (lock === true) {
             setIndex(index + 1);
             setQuestion(quiz[index]);
             setLock(false);
-            option_array.map((option)=>{
+            option_array.map((option) => {
                 option.current.classList.remove("wrong");
                 option.current.classList.remove("correct");
                 return null;
-            })
+            });
         }
-    }
+    };
 
-    const reset = ()=>{
+    const reset = () => {
         setIndex(0);
         setQuestion(quiz[index]);
         setScore(0);
         setLock(false);
         setResult(false);
-    }
+    };
 
     return (
         <div className="container">
@@ -60,7 +61,14 @@ export function Quiz(){
             {
                 result ? (
                     <>
-                        <h2>You scored {score}</h2>
+                        <motion.h2
+                            initial={{ opacity: 0, y: -20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 20 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            You scored {score}
+                        </motion.h2>
                         <button onClick={reset}>Reset</button>
                     </>
                 ) : (
@@ -79,5 +87,4 @@ export function Quiz(){
             }
         </div>
     );
-    
 }
